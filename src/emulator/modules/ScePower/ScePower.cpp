5c9ec8c61a09935a6189c0c5e3943aa7aceb7896
Talkashie <26035046+Talkashie@users.noreply.github.com>
2018-03-09 23:40:04 -0600

    Update ScePower.cpp
    Set low battery warning to false, and set charging value to true. Testing with "FlppyBird" homebrew.

--- src/emulator/modules/ScePower/ScePower.cpp
+++ src/emulator/modules/ScePower/ScePower.cpp
@@ -100,7 +100,7 @@ EXPORT(int, scePowerGetUsingWireless) {
 }
 
 EXPORT(int, scePowerIsBatteryCharging) {
-    return unimplemented("scePowerIsBatteryCharging");
+    return SCE_TRUE;
 }
 
 EXPORT(int, scePowerIsBatteryExist) {
@@ -108,7 +108,7 @@ EXPORT(int, scePowerIsBatteryExist) {
 }
 
 EXPORT(int, scePowerIsLowBattery) {
-    return unimplemented("scePowerIsLowBattery");
+    return SCE_FALSE;
 }
 
 EXPORT(int, scePowerIsLowBatteryInhibitUpdateDownload) {
